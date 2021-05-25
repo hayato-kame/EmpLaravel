97d1a9dca626b7f79c095e30fe51b30504f748c7
PH Kame <phkame@kameyamamisanoiMac.local>
2021-05-25 18:50:55 +0900

    departments table create

---
+++ database/migrations/2021_05_25_181839_create_departments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            // 主キーは、stringです employeesの親テーブルです
+            $table->string('department_id', 20)->primary();
+            $table->string('department_name', 20);
+            // timestamps()を利用しない
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
